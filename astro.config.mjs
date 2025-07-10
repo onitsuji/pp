f6ebcc9e5106e0ccf2d297a9e458e55e4cc0c241
justin-0 <itsjustinoconnell@gmail.com>
2025-07-10 12:39:05 +0100

    import layout case

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
 
 import mdx from "@astrojs/mdx";
 
@@ -9,7 +8,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-
   vite: {
     plugins: [tailwindcss()],
   },
